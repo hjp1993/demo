1f62dd135eb31e68ee284578ba8c7d5495251134
hjp1993 <840426804@qq.com>
2018-08-26 11:07:39 +0800

    创建测试控制器

---
+++ web/controller/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hjp1993
+ * Date: 2018/8/26
+ * Time: 11:04
+ */
+namespace web\controller;
+
+class Index{
+    public function show()
+    {
+        echo "hjp";
+    }
+}
